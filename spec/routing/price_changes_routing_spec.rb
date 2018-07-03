d793206e0de8731749435c10b3078b485a55b431
Girish Gopaul <ggopaul0510@gmail.com>
2018-07-03 14:46:29 +0400

    Correction

--- spec/routing/price_changes_routing_spec.rb
+++ spec/routing/price_changes_routing_spec.rb
@@ -3,27 +3,33 @@ require 'rails_helper'
 RSpec.describe PriceChangesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/price_changes').to route_to('price_changes#index')
+      expect(get: '/price_changes')
+        .to route_to('price_changes#index')
     end
 
     it 'routes to #show' do
-      expect(get: '/price_changes/1').to route_to('price_changes#show', id: '1')
+      expect(get: '/price_changes/1')
+        .to route_to('price_changes#show', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/price_changes').to route_to('price_changes#create')
+      expect(post: '/price_changes')
+        .to route_to('price_changes#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/price_changes/1').to route_to('price_changes#update', id: '1')
+      expect(put: '/price_changes/1')
+        .to route_to('price_changes#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/price_changes/1').to route_to('price_changes#update', id: '1')
+      expect(patch: '/price_changes/1')
+        .to route_to('price_changes#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/price_changes/1').to route_to('price_changes#destroy', id: '1')
+      expect(delete: '/price_changes/1')
+        .to route_to('price_changes#destroy', id: '1')
     end
   end
 end
