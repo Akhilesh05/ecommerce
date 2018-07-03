04bb809b4d54c5c5330b1f5ac8e673d6f2bbab0f
Girish Gopaul <ggopaul0510@gmail.com>
2018-07-03 11:24:37 +0400

    Added nested resources and dependent: :destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :addresses
-  resources :users
+  resources :users do
+    resources :addresses
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
