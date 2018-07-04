d127852950917c182619574abf6ed8354173c2af
Girish Gopaul <ggopaul0510@gmail.com>
2018-07-04 14:20:39 +0400

    Updated user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email
+  attributes(
+    :id,
+    :first_name,
+    :last_name,
+    :email,
+    :mobile_number,
+    :created_at,
+    :updated_at
+  )
   # has_many :addresses
 end
