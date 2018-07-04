b9370bd32b407dc82ea1acb312155e0056fa0ec1
Girish Gopaul <g.gopaul@outlook.com>
2018-07-04 11:43:19 +0400

    Added JSON serializer (#7)
    * Added JSON serializer
    
    * Correction of Gemfile order

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,6 @@ class User < ApplicationRecord
   before_save :digest_password, if: proc { password.present? }
   attr_accessor :password
 
-  def as_json(options = {})
-    options[:except] ||= %i[password_digest password_salt]
-    super(options)
-  end
-
   def digest_password
     self.password_salt = SecureRandom.uuid
     digest = Digest::SHA2.new(512)
