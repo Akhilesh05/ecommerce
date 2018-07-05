b6c7d866e7c61eeb77a68a5baafc1490fb16fdc6
Girish Gopaul <g.gopaul@outlook.com>
2018-07-05 20:52:24 +0400

    Some nice testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  include ActiveModel::Dirty
-
-  has_many :addresses, dependent: :destroy
-  before_save :digest_password, if: proc { password.present? }
   attr_accessor :password
+
+  # Associations
+  has_many :addresses, dependent: :destroy
+  # Validations
+  validates :first_name, :last_name, :email, :password_digest, :mobile_number, presence: true
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :mobile_number, length: { is: 8 }
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: /.@.+\../
+  validates :nic_number,
+            :passport_country_number,
+            uniqueness: { case_sensitive: false, allow_nil: true }
+  # Hooks
+  before_validation :digest_password, if: proc { password.present? }
+  # Scopes
 
   def digest_password
     self.password_salt = SecureRandom.uuid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association tests
+  it { should have_many(:addresses).dependent(:destroy) }
+
+  # Validation tests
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:mobile_number) }
+  it { should validate_length_of(:mobile_number).is_equal_to(8) }
+  it { should validate_length_of(:first_name).is_at_least(2) }
+  it { should validate_length_of(:last_name).is_at_least(2) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_uniqueness_of(:nic_number).case_insensitive.allow_nil }
+  it { should validate_uniqueness_of(:passport_country_number).case_insensitive.allow_nil }
+  it { should allow_value('user@example.com').for(:email) }
+  it { should_not allow_value('user@example').for(:email) }
 end
