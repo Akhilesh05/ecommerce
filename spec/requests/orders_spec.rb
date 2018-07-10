407884d7031ea17d8aeddae236be79777d5d7140
Girish Gopaul <ggopaul0510@gmail.com>
2018-07-10 18:00:13 +0400

    Still doing request spec for orders

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -3,5 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Orders', type: :request do
-  pending
+  subject { response }
+
+  let(:user) { User.first }
+  let(:address) { user.addresses.first }
+
+  before do
+    create_list(:user, 2).each do |usr|
+      create_list(:address, 3, user_id: usr.id).each do |addr|
+        create_list :order, 4, address_id: addr.id
+      end
+    end
+  end
+
+  describe 'GET /orders' do
+    before { get orders_path, headers: valid_auth_header }
+
+    it { is_expected.to have_http_status :ok }
+    it { expect(json_response.length).to eq user.orders.count }
+  end
+
+  describe 'GET /addresses/:id/orders' do
+    before { get address_orders_path(address), headers: valid_auth_header }
+
+    it { is_expected.to have_http_status :ok }
+    it { expect(json_response.length).to eq address.orders.count }
+  end
+
+  describe 'POST /addresses/:id/orders' do
+    before { post address_orders_path(address), headers: valid_auth_header }
+
+    it { is_expected.to have_http_status :created }
+  end
 end
