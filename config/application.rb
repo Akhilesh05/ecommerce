84ac6e0d760bc9e1a5213de0341ef5d295e379b2
Girish Gopaul <g.gopaul@outlook.com>
2018-07-12 10:36:37 +0400

    Just a comment mate

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module Ecommerce
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # config.active_job.queue_adapter = :sidekiq
+
     if Rails.env.production?
       config.eager_load_paths << Rails.root.join('lib')
     else
