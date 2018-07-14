613dff6b2e35107751ef0ad510491a15be89af70
Girish Gopaul <g.gopaul@outlook.com>
2018-07-14 09:20:18 +0400

    YAML returns false if file empty, correction of bug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,7 +9,7 @@
 Sidekiq.configure_server do
   schedule_file = 'config/schedule.yml'
 
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file) || {}) if File.exist?(schedule_file)
 end
 
 # Sidekiq.configure_client do
