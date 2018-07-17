097d360e7d2d32d9638216e8ca398bc73851182a
Girish Gopaul <g.gopaul@outlook.com>
2018-07-17 09:11:46 +0400

    Small minor change in car 🚙 🚘 🚗

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -3,6 +3,7 @@
 class EmailWorker
   include Sidekiq::Worker
   sidekiq_options retry: 1 # tries once and dies
+  sidekiq_retry_in { |retry_num| 60 * (retry_num + 1) - 30 * retry_num }
 
   def perform(user_id)
     user = User.find user_id
