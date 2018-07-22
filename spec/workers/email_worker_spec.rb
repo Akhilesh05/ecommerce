647b9acbecfd37eea59337c816f3129e8e831b2e
Girish Gopaul <g.gopaul@outlook.com>
2018-07-22 10:26:00 +0400

    Small change to test

--- spec/workers/email_worker_spec.rb
+++ spec/workers/email_worker_spec.rb
@@ -5,11 +5,14 @@ require 'rails_helper'
 RSpec.describe EmailWorker, type: :worker do
   let(:user) { create :user }
 
-  before do
-    Sidekiq::Testing.inline!
-    allow(UserMailer).to receive(:with).and_call_original
-    EmailWorker.perform_async user.id
-  end
+  # before { Sidekiq::Testing.inline! }
 
-  it { expect(UserMailer).to have_received(:with).with(user: user) }
+  describe '.perform' do
+    before do
+      allow(UserMailer).to receive(:with).and_call_original
+      EmailWorker.perform_async user.id
+    end
+
+    it { expect(UserMailer).to have_received(:with).with(user: user) }
+  end
 end
